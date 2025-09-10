7008e373793fe594728590357669deb0215803a1
dharmikvarsani <dharmikvarsani968@gmail.com>
2025-09-10 15:28:43 +0530

    fix issue

--- src/app/api/auth/me/route.js
+++ src/app/api/auth/me/route.js
@@ -2,11 +2,12 @@ import { NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 import { connectDB } from "@/lib/db";
 import { User } from "@/model/user";
+import { cookies } from "next/headers"; 
 
-export async function GET(req) {
+export async function GET() {
   await connectDB();
 
-  const token = req.cookies.get("token")?.value;
+  const token = cookies().get("token")?.value;
 
   if (!token) {
     return NextResponse.json({ user: null }, { status: 200 });
